ebff8ec6dee5315ed54b0fa440e67852b80db0fa
Andrii Zavada <andrii.zavada@gmail.com>
2016-11-15 10:17:18 +0200

    added page action

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@
             if (items["provider"] === "dropbox") {
                 var provider = new DropBoxProvider(items["dropboxAccessToken"]);
                 provider.downloadFile(items["dropboxFileId"], function (data) {
-                    // chrome.storage.local.set({dbFile: data});
-                    // parseKDBX(data);
+                    parseKDBX(data);
                 });
             }
         });
@@ -29,13 +28,45 @@
             var credentials = new kdbxweb.Credentials(kdbxweb.ProtectedValue.fromString('qwerty'), undefined);
             kdbxweb.Kdbx.load(dataAsString, credentials, function(db) {
                 console.log(db);
+                var fields = db.groups[0].entries[0].fields;
+
+                console.log(fields);
+                var bytes = fields.Password._value;
+                var salt = fields.Password._salt;
+                var password = new kdbxweb.ProtectedValue(bytes, salt);
+                console.log(password.getText());
             });
         } catch (e) {
             console.log(e);
         }
-
     }
 
     loadOptions();
     addMessageListener();
+
+    chrome.pageAction.onClicked.addListener(function (tab) {
+       console.log(tab);
+    });
+
+    var rule1 = {
+        conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { schemes: ['https', "http"] }
+            })
+        ],
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+    };
+
+    // chrome.runtime.onInstalled.addListener(function(details)
+    // {
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+            chrome.declarativeContent.onPageChanged.addRules([rule1]);
+        });
+    // });
+
+    chrome.runtime.onMessage.addListener(function(request, sender, responseCallback) {
+        if (typeof request.log !== "undefined") {
+            console.log(request.log);
+        }
+    });
 })();
